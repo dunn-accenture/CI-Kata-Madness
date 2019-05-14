b76e905dfa551c8d70ec23946595dea4b4df5384
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-05-14 10:29:44 -0400

    repull the thang

---
+++ DotNet/CIKataFrontEnd/CIKataFrontEnd.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,48 @@
+using System;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+using Shouldly;
+
+
+namespace CIKataFrontEnd.IntegrationTests
+{
+    public class IntegrationTests
+    {
+        private string _driverFile = SetChromeDriver();
+
+        private static string SetChromeDriver()
+        {
+            var envVariable = Environment.GetEnvironmentVariable("ChromeDriverFile");
+
+            if (envVariable != null)
+            {
+                return envVariable;
+            }
+
+            return "chromedrivermac";
+        }
+
+        [Fact]
+        public void HomePageIsCorrect()
+        {
+            var service = ChromeDriverService.CreateDefaultService(Environment.CurrentDirectory, _driverFile);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+            var webDriver = new ChromeDriver(service, chromeOptions);
+            
+            webDriver.Navigate().GoToUrl("http://localhost:5000/");
+
+            var div = webDriver.FindElementByTagName("div");
+
+            try
+            {
+                div.Text.ShouldBe("This is a successful call!");
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+
+        }
+    }
+}
